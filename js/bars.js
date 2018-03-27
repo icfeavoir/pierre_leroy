0cf1d76a99bad316cb84f7bd27da565757114789
Pierre Leroy <pierrotleroy@hotmail.fr>
2018-03-27 22:25:17 +0200

    modif barres

--- js/bars.js
+++ js/bars.js
@@ -1,30 +1,10 @@
 $(document).ready(function(){
 
 	$('.bar').each(function(){
-		var id = $(this).attr('id');
+		var bar = $(this);
+		var id = bar.attr('id');
 		inView("#"+id).on('enter', function(){
-		    	activeBar($('#'+id));
+		    	bar.css('max-width', bar.attr('value')+'%');
 		    });
 	});
-
-	// $('.bar').each(function(){
-	// 	var bar = $(this);
-	// 	bar.on('show', function(){
-	// 		console.log('ok');
-	// 		activeBar($(this));
-	// 	});
-	// });
 });
-
-function activeBar(bar){
-	var actual = bar.width();
-	var max = parseInt(bar.attr('value'));
-	var idInt = setInterval(function(){
-		if(actual >= max){
-			clearInterval(idInt);
-		}else{
-			actual++;
-			bar.width(actual+'%');
-		}
-	}, 15);
-}
